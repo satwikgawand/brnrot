23fb6d49ca9747eb6d5b929bd44c3074b6f49678
satwikgawand <satwikgawand@gmail.com>
2026-03-31 21:46:52 +0800

    chore: remove updates section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import Hero from '../components/Hero'
 import ProjectGrid from '../components/ProjectGrid'
-import UpdatesFeed from '../components/UpdatesFeed'
 import Footer from '../components/Footer'
 import projects from '../../data/projects.json'
 
@@ -9,7 +8,6 @@ export default function HomePage() {
     <div className="min-h-screen bg-[#0c0c0c] text-gray-100 animate-fade-in">
       <Hero />
       <ProjectGrid projects={projects} />
-      <UpdatesFeed />
       <Footer />
     </div>
   )
